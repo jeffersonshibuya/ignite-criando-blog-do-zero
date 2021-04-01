c6e34f94ecedf56086a3da828008d01b3cfb5534
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2021-04-01 16:25:57 -0300

    Utterances multi load fixed

--- src/components/UtterancesComments/index.tsx
+++ src/components/UtterancesComments/index.tsx
@@ -1,18 +1,37 @@
-export const UtterancesComments: React.FC = () => (
-  <section
-    ref={elem => {
-      if (!elem) {
-        return;
-      }
-      const scriptElem = document.createElement('script');
-      scriptElem.src = 'https://utteranc.es/client.js';
-      scriptElem.async = true;
-      scriptElem.crossOrigin = 'anonymous';
-      scriptElem.setAttribute('repo', 'jeffersonshibuya/ignite-blog-utteranc');
-      scriptElem.setAttribute('issue-term', 'pathname');
-      scriptElem.setAttribute('label', 'blog-comment');
-      scriptElem.setAttribute('theme', 'github-dark');
-      elem.appendChild(scriptElem);
-    }}
-  />
-);
+import { useEffect, useRef } from 'react';
+
+export const UtterancesComments: React.FC = () => {
+  const commentsDiv = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (commentsDiv) {
+      const scriptEl = document.createElement('script');
+      scriptEl.setAttribute('src', 'https://utteranc.es/client.js');
+      scriptEl.setAttribute('crossorigin', 'anonymous');
+      scriptEl.setAttribute('async', 'true');
+      scriptEl.setAttribute('repo', 'jeffersonshibuya/ignite-blog-utteranc');
+      scriptEl.setAttribute('issue-term', 'pathname');
+      scriptEl.setAttribute('label', 'blog-comment');
+      scriptEl.setAttribute('theme', 'github-dark');
+      commentsDiv.current.appendChild(scriptEl);
+    }
+  }, []);
+
+  return <div ref={commentsDiv} />;
+};
+// <section
+//   ref={elem => {
+//     if (!elem) {
+//       return;
+//     }
+//     const scriptElem = document.createElement('script');
+//     scriptElem.src = 'https://utteranc.es/client.js';
+//     scriptElem.async = true;
+//     scriptElem.crossOrigin = 'anonymous';
+//     scriptElem.setAttribute('repo', 'jeffersonshibuya/ignite-blog-utteranc');
+//     scriptElem.setAttribute('issue-term', 'pathname');
+//     scriptElem.setAttribute('label', 'blog-comment');
+//     scriptElem.setAttribute('theme', 'github-dark');
+//     elem.appendChild(scriptElem);
+//   }}
+// />
